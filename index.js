21a8383fd68ef0f2b797f444c1eadb2643370da1
Andrey Chernykh <andrey.chernykh@dasha.ai>
2021-12-01 14:18:17 +0700

    fix inbound calls capturing

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ commander
   .action(async ({ phone, config }) => {
     const app = await dasha.deploy("./app");
 
-    app.connectionProvider = () => dasha.sip.connect(new dasha.sip.Endpoint(config));
-    app.ttsDispatcher = () => "dasha";
-
     app.setExternal("checkPin", ({ pin }) => pin.includes("1"));
 
     await app.start();
 
     const conv = app.createConversation({ phone });
+    conv.audio.tts = "dasha"
+    conv.sip.config = config;
     const result = await conv.execute();
     console.log(`pin is ${result.output.pincode}`);
 
@@ -28,15 +27,13 @@ commander
   .command("in")
   .description("check calls to Dasha")
   .action(async () => {
-    const app = await dasha.deploy("./app");
-
-    app.connectionProvider = () => dasha.sip.connect(new dasha.sip.Endpoint("default"));
-    app.ttsDispatcher = () => "dasha";
+    const app = await dasha.deploy("./app", { groupName: "Default" });
 
     app.setExternal("checkPin", ({ pin }) => pin.includes("1"));
 
-    app.queue.on("ready", async (conv) => {
-      conv.input = { phone: null };
+    app.queue.on("ready", async (id, conv, info) => {
+      conv.input = { phone: "" };
+      conv.audio.tts = "dasha";
       const result = await conv.execute();
 
       console.log(`pin is ${result.output.pincode}`);
